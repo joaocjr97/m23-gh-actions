cf29a35e5f66335417ec14749b553a450a9f3041
joao carlos <163042743+d4sjoao@users.noreply.github.com>
2025-06-24 20:41:57 -0300

    preparação de ambiente

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,14 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'mochawesome-report',
+    overwrite: false,
+    reportFilename: "index.html",
+    html: true,
+    json: true
+  }
 });
